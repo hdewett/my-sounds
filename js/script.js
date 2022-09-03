61722e88fa05f3e21f7ccd1b5f79bb4f2836e6a3
hdewett <105822645+hdewett@users.noreply.github.com>
2022-09-02 21:50:23 -0400

    added embedded spotify players to list

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const myMusicList = [
-  "American Honey - Current Joys",
-  "Way it Goes - Hippocampus",
-  "I Don't Want to Talk - Wallows",
-  "Northern Boy - The Academic",
-  "Cherry - Harry Styles",
-  "Cemetery - COIN",
-  "My Type - Saint Motel",
-  "cold/mess - Prateek Kuhad",
-  "Everybody Talks - Neon Trees",
-  "Cough Syrup - Young the Giant",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/4HOsVv1uK6DS0gYKBCWMGv?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/6CES1554HcWMIVroJfmmBm?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/1KO4aiGkvF1eMTBrzL4vAd?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/5O918Wom2bZeRPScMG5Jtd?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/2IOFZdYYkFxEHVz1w34PoL?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/5czZkLGepjburbkbEaCfm4?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/2DFi8rwHcVkTTgu7PULhtI?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/7uhINGViZPygI2AljxO8KN?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/2iUmqdfGZcHIhS3b9E9EWq?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+  "<iframe style='border-radius:12px' src='https://open.spotify.com/embed/track/1UqhkbzB1kuFwt2iy4h29Q?utm_source=generator' width='100%' height='80' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
 ];
 
 const mixList = document.querySelector(".my-songs");
